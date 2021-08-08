00bca96da030f10e6a91cf69193bc6bcba266737
nguyen.van.taib <nguyen.van.taib@b122570-mb.local>
2021-08-08 18:44:45 +0700

    test

--- src/utils/arrayFilter.js
+++ src/utils/arrayFilter.js
@@ -1,11 +1,3 @@
-/**
- * loc ra cac phan tu trong mang co chua tu tuong tu target
- * neu khong tim duoc tra ve false
- * neu tim duoc tra ve 5 phan tu dau tien
- * @param {array} array 
- * @param {string} target 
- * @returns 
- */
 export default function getFilteredArray(array, target) {
   if (target) {
     let arrayFiltered = array.filter(
--- src/utils/category.js
+++ src/utils/category.js
@@ -1,31 +1,20 @@
-/**
- * Copy cac truong filter category cua global state co level khong lon hon
- * @param {object} filter 
- * @param {number} level 
- * @returns 
- */
 export function getCategoryFilterData(filter, level) {
   let newCategory = {};
-  for( let index = 0; index <= level; index ++){
-    newCategory[`categoryLvl${index}`] = filter[`hierarchicalCategories.lvl${index}_like`];
+  for (let index = 0; index <= level; index++) {
+    newCategory[`categoryLvl${index}`] =
+      filter[`hierarchicalCategories.lvl${index}_like`];
   }
   return newCategory;
 }
 
-/**
- * An cac category level 1
- */
-export function hideCategoryLvl1(){
-    document.querySelectorAll('li.category__item.lvl1').forEach(
-        element => element.style.display = 'none'
-    );
+export function hideCategoryLvl1() {
+  document
+    .querySelectorAll("li.category__item.lvl1")
+    .forEach((element) => (element.style.display = "none"));
 }
 
-/**
- * Hien thi cac category level 1
- */
-export function showCategoryLvl1(){
-    document.querySelectorAll('li.category__item.lvl1').forEach(
-        element => element.style.display = 'block'
-    );
+export function showCategoryLvl1() {
+  document
+    .querySelectorAll("li.category__item.lvl1")
+    .forEach((element) => (element.style.display = "block"));
 }
